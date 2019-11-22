957337937a6d3d71c789af0b0bd1eee8ce91c2f5
Nicholas <>
2019-11-22 14:40:10 -0500

    Created the function to get the new file name of a song

--- WAV to MP3 Converter.py
+++ WAV to MP3 Converter.py
@@ -1,9 +1,13 @@
 import os
-from pathlib import Path
 
-
+# User variables here:
 INPUT_DIR = "input"
 OUTPUT_DIR = "output"
+
+# End of user variables
+
+INPUT_DIR = os.path.abspath(INPUT_DIR)
+OUTPUT_DIR = os.path.abspath(OUTPUT_DIR)
 
 
 # Returns a list of all file paths inside a parent directory recursively, does not return folders
@@ -11,7 +15,6 @@ def get_all_files_in_dir(parent_path):
     all_files = []
     for path in os.listdir(parent_path):
         path = os.path.join(parent_path, path)
-        path = os.path.abspath(path)
         if os.path.isfile(path):
             all_files.append(path)
         elif os.path.isdir(path):  # This is a directory, so get files inside of it
@@ -23,18 +26,25 @@ def filter_to_wav(file_paths):
     wav = []
     for path in file_paths:
         name, ext = os.path.splitext(path)
-        print(ext)
         if ext == ".wav":
             wav.append(path)
     return wav
 
 
+# Gets the full path to the new mp3 file given a wav file and the output directory where the new file should be saved
+def get_new_file_name(old_file_name, output_directory):
+    old_file_name_without_drive = os.path.splitdrive(old_file_name)[1]
+    old_file_name_without_extension = os.path.splitext(old_file_name_without_drive)[0]
+    partial_new_file_name = old_file_name_without_extension + ".mp3"
+    new_file_name = output_directory + partial_new_file_name
+    return new_file_name
+
 
 all_files = get_all_files_in_dir(INPUT_DIR)
 
 all_wav = filter_to_wav(all_files)
-for i in all_wav:
-    print(i)
+for i in all_wav[:1]:
+    print(get_new_file_name(i, OUTPUT_DIR))
 
 
 
